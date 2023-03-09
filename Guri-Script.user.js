d6c3385904bd4b24d6a7da5ad430741e43d79051
Auri Gabriel <ririmello.a@gmail.com>
2023-03-09 11:41:15 -0300

    Update Guri-Script.user.js

--- Guri-Script.user.js
+++ Guri-Script.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Guri-Script
-// @version      2.1.0
+// @version      2.2.0
 // @description  Remove a segunda parte da string de todos os botões com a função detalhesTurma() na página de solicitar matrícula no Guri e adiciona um botão para expandir todos os detalhes.
 // @match        https://guri.unipampa.edu.br/pta/solicitar_matricula/
 // @grant        none
@@ -20,24 +20,34 @@
   
     // Function to expand all details
     function expandAll() {
-        document.querySelectorAll('a[href^="javascript:detalhesTurma("]').forEach(function(link) { 
-            link.click();
-        });
+        let button = document.querySelector("#expand-all-button");
+        if (button.textContent === "Expandir todos os detalhes") {
+            button.textContent = "Recolher todos os detalhes";
+            document.querySelectorAll('a[href^="javascript:detalhesTurma("]').forEach(function(link) { 
+                link.click();
+                link.dataset.expanded = "true";
+            });
+        } else {
+            button.textContent = "Expandir todos os detalhes";
+            document.querySelectorAll('a[href^="javascript:detalhesTurma("][data-expanded="true"]').forEach(function(link) { 
+                link.click();
+                link.dataset.expanded = "false";
+            });
+        }
     }
 
     // Add a button to expand all the details
     let button = document.createElement("button");
-    button.innerHTML = "Expandir todos os detalhes";
+    button.id = "expand-all-button";
+    button.textContent = "Expandir todos os detalhes";
     button.onclick = expandAll;
-  
   
     let headerTitles = document.querySelectorAll('.card-header b.card-title');
     headerTitles.forEach((header) => {
-      console.log(header.parentElement);
-      if (header && header.innerText === "TURMAS OFERTADAS DA SUA GRADE CURRICULAR") {
-          let cardHeader = header.parentElement; 
-          cardHeader.appendChild(button);
-          cardHeader.classList.add("d-flex", "justify-content-between");
-      }
+        if (header && header.innerText === "TURMAS OFERTADAS DA SUA GRADE CURRICULAR") {
+            let cardHeader = header.parentElement; 
+            cardHeader.appendChild(button);
+            cardHeader.classList.add("d-flex", "justify-content-between");
+        }
     });
 })();
