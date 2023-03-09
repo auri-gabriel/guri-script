a862d475c2fbf867792842f7409735188485cd7c
Auri Gabriel <ririmello.a@gmail.com>
2023-03-09 11:37:20 -0300

    Update and rename Arruma o mostrar detalhes da solicitacao de matricula.user.js to Guri-Script.user.js

--- Guri-Script.user.js
+++ Guri-Script.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Guri-Script
-// @version      2
+// @version      2.1.0
 // @description  Remove a segunda parte da string de todos os botões com a função detalhesTurma() na página de solicitar matrícula no Guri e adiciona um botão para expandir todos os detalhes.
 // @match        https://guri.unipampa.edu.br/pta/solicitar_matricula/
 // @grant        none
@@ -12,6 +12,10 @@
     // Remove the second part of the href attribute from all links with a function named detalhesTurma
     document.querySelectorAll('a[href^="javascript:detalhesTurma("]').forEach(function(link) {
         link.setAttribute('href', link.getAttribute('href').replace(/\$\('#div_dados_sm_\d+'\)\.toggle\(\);/, ''));
+        link.dataset.expanded = "false";
+        link.onclick = function() {
+          this.dataset.expanded = this.dataset.expanded === "true" ? "false" : "true";
+        };
     });
   
     // Function to expand all details
